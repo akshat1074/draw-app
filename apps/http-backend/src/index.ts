b1fb251ac995915d4b127b663a57737ee97a2b57
akshat_1074 <valaniakshat1074@gmail.com>
2025-12-31 07:57:05 +0530

    Initial

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express"
+
+const app = express()
+
+app.post("/signup",(req,res)=>{
+    
+})
+
+app.post("/signup",(req,res)=>{
+    
+})
+
+app.post("/signup",(req,res)=>{
+    
+})
+
+app.post("/signup",(req,res)=>{
+    
+})
+
