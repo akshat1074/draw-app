2cd99fe908fcbef3b6b1061fa20674d494a8afa7
akshat_1074 <valaniakshat1074@gmail.com>
2026-01-09 07:43:13 +0530

    http routes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import jwt from "jsonwebtoken"
-import {CreateUserSchema,SiginSchem,CreateRoomSchema} from "@repo/common/types";
+import {CreateUserSchema,SiginSchema,CreateRoomSchema} from "@repo/common/types";
 import cors from "cors";
 import { prismaClient } from "@repo/db/client";
+import { middleware } from "./middleware";
 
 
 const app = express()
@@ -32,21 +33,106 @@ app.post("/signup", async(req,res)=>{
         })
     } catch(e){
         res.status(411).json({
-            userId: user.id
+            message:"User already exits with this username"
 
         })
     }
 })
 
-app.post("/signup",(req,res)=>{
-    
+app.post("/signin", async (req,res)=>{
+    const parsedData = SiginSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
+    const user = await prismaClient.user.findFirst({
+        where:{
+            email:parsedData.data.username,
+            password:parsedData.data.password
+        }
+})
+if(!user){
+    res.status(403).json({
+        message:"Not authorized"
+    })
+    return;
+}
+
+ const token = jwt.sign({
+    userId:user?.id
+ },JWT_SECRET);
+
+ res.json({
+    token
+ })
 })
 
-app.post("/signup",(req,res)=>{
-    
+app.post("/room",middleware, async(req,res)=>{
+    const parsedData = CreateRoomSchema.safeParse(req.body)
+    if(!parsedData.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
+    const userId = req.userId;
+
+    try{
+        const room = await prismaClient.room.create({
+            data:{
+                slug:parsedData.data.name,
+                adminId:userId
+            }
+        })
+        res.json({
+            roomId:room.id
+        })
+    }catch(e){
+        res.status(411).json({
+            messgae:"Room already exists with this name"
+        })
+    }
 })
 
-app.post("/signup",(req,res)=>{
-    
+app.get("/chats/:roomId",(req,res)=>{
+    try{
+        const roomId = Number(req.params.roomId);
+        console.log(req.params.roomId);
+        const messages = await prismaClient.chat.findMany({
+            where:{
+                roomId:roomId
+            },
+            orderBy:{
+                id:"desc"
+            },
+            take:1000
+        });
+
+        res.json({
+            messages
+        })
+    } catch(e){
+        console.log(e);
+        res.json({
+            messages:[]
+        })
+    }
 })
+
+app.get("/room/:slug",async(res,req)=>{
+    const slug = req.params.slug;
+    const room = await prismaClient.room.findFirst({
+        where:{
+            slug
+        }
+    });
+
+    res.json({
+        room
+    })
+})
+
+app.listen(3001)
 
